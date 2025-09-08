90fa2b188c5323c71b541597a15829a3feaa6a88
Sandip Bhalodiya <bhalodia.sandip@gmail.com>
2025-09-08 13:31:43 +0530

    day-1 commit

---
+++ 01-basics/index.py
@@ -0,0 +1,89 @@
+import math
+# print("Hello World")
+
+# x = 10
+# y = 10.5
+# name = 'John'
+# is_active = False
+
+# print(type(x))
+# print(type(y))
+# print(type(name))
+# print(type(is_active))
+
+# username = input("Please enter your name: ")
+# print('Hi', username)
+
+# a,b=10,3
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+# print(a > b)
+# print(a < b)
+
+# x,y = True, False
+# print(x and y)
+# print(x or y)
+# print(not x)
+
+# s = "Python Basics"
+# print(s)
+# print(s[3:])
+# print(s[:9])
+# print(s[3:9])
+# print(s[:-3])
+# print(s[::-1])
+# print(len(s))
+
+#string object methods. it will start from s. (variable.) because class methods not like len
+# print(s.upper())
+# print(s.lower())
+# print(s.replace("y", "K"))
+
+# numbers = [1,2,2,3,3,3,4,5]
+# print(numbers[4])
+# numbers.append('Om')
+# print(numbers)
+# numbers.remove(3) # remove first occurence of value
+# print(numbers)
+
+# t = (1, 2, 3)
+# print(t[0])     # 
+# t[0] = 'k' # not mutable throw error
+
+# d = {"name": "snadip", "age": 22}
+# d['age'] = 40
+# print(d)
+
+# age = 62
+# if(age < 18):
+#     print('Half ticket: Child')
+# elif(age >= 18 and age <= 60):
+#     print('Full ticket')
+# else:
+#     print("Half ticket: senior citizen")
+
+# for i in range(5):
+#     print(i)
+
+# n=3
+# while(n > 0):
+#     print(n)
+#     n -= 1
+
+# def greet(name):
+#     return "hello " + name
+# print(greet("sandip"))
+
+# try:
+#     x = 10 / 0
+# except ZeroDivisionError:
+#     print("Devided by zero")    
+# finally:
+#     print("Done")    
+
+print(math.sqrt(16))
